593899882e08bbaeab55e8a4d5a34283090d9e9e
Cristel Chandre <cchandre@gmail.com>
2021-06-17 12:59:36 +0200

    Update kam_conf.py
    - option: computation of error norm using a finer grid (2n) with the entry self.finer_grid (True or False)

--- kam_conf.py
+++ kam_conf.py
@@ -1,21 +1,21 @@
 import numpy as xp
 from numpy import linalg as LA
-from numpy.fft import fftn, ifftn, fftfreq
+from numpy.fft import fftn, ifftn, fftfreq, fftshift, ifftshift
 import convergence as cv
 import time
 import warnings
 warnings.filterwarnings("ignore")
 
 def main():
-	# dict_params = {
-	# 	'n': 2 ** 12,
-	# 	'omega0': [0.618033988749895, -1.0],
-	# 	# 'eps_region': [[0, 0.35], [0, 0.12]],
-	# 	'eps_region': [[0.0, 0.03], [0.0, 0.03]],
-	# 	'eps_modes': [1, 1],
-	#   'eps_dir' : [1, 1],
-	# 	'Omega': [1.0, 0.0],
-	# 	'potential': 'pot1_2d'}
+	dict_params = {
+		'n': 2 ** 11,
+		'omega0': [0.618033988749895, -1.0],
+		# 'eps_region': [[0, 0.35], [0, 0.12]],
+		'eps_region': [[0.0, 0.03], [0.0, 0.03]],
+		'eps_modes': [1, 1],
+		'eps_dir' : [1, 1],
+		'Omega': [1.0, 0.0],
+		'potential': 'pot1_2d'}
 	# dict_params = {
 	# 	'n': 2 ** 10,
 	# 	'omega0': [0.414213562373095, -1.0],
@@ -28,26 +28,28 @@ def main():
 	# 	'eps_region': [[0, 0.06], [0, 0.2]],
 	# 	'Omega': [1.0, 0.0],
 	# 	'potential': 'pot1_2d'}
-	dict_params = {
-		'n': 2 ** 8,
-		'omega0': [1.324717957244746, 1.754877666246693, 1.0],
-		'eps_region': [[0.0, 0.35], [0.0,  0.40], [0.1, 0.1]],
-		'eps_modes': [1, 1, 0],
-		'eps_dir': [1, 5, 0.1],
-		'Omega': [1.0, 1.0, -1.0],
-		'potential': 'pot1_3d'}
+	# dict_params = {
+	# 	'n': 2 ** 7,
+	# 	'omega0': [1.324717957244746, 1.754877666246693, 1.0],
+	# 	'eps_region': [[0.0, 0.15], [0.0,  0.40], [0.1, 0.1]],
+	# 	'eps_modes': [1, 1, 0],
+	# 	'eps_dir': [1, 5, 0.1],
+	# 	'Omega': [1.0, 1.0, -1.0],
+	# 	'potential': 'pot1_3d'}
 	dict_params.update({
+	    'tolmin': 1e-9,
+	    'threshold': 1e-11,
+		'tolmax': 1e30,
+		'maxiter': 50,
+		'precision': 128,
 		'eps_n': 256,
 		'eps_indx': [0, 1],
 		'eps_type': 'cartesian',
-		'tolmax': 1e10,
-		'tolmin': 1e-8,
-		'maxiter': 50,
-		'threshold': 1e-11,
 		'dist_surf': 1e-5,
-		'precision': 64,
 		'choice_initial': 'continuation',
-		'save_results': False,
+		'finer_grid': True,
+		'r': 4,
+		'save_results': True,
 		'plot_results': True})
 	dv = {
 		'pot1_2d': lambda phi, eps, Omega: Omega[0] * eps[0] * xp.sin(phi[0]) + eps[1] * (Omega[0] + Omega[1]) * xp.sin(phi[0] + phi[1]),
@@ -63,7 +65,7 @@ def main():
 	# epsilon[:, 1] = radii * xp.sin(theta)
 	# if len(eps_region[:, 0]) >= 3:
 	# 	epsilon[:, 2:] = eps_region[2:, 0]
-	# datanorm = cv.line(epsilon,case, [True, 4], display=True)
+	# datanorm = cv.line(epsilon,case, [True, case.r], display=True)
 
 	eps_region = xp.array(case.eps_region)
 	eps_modes = xp.array(case.eps_modes)
@@ -71,22 +73,24 @@ def main():
 	epsilon0 = eps_region[0, 0]
 	epsvec = epsilon0 * eps_modes * eps_dir + (1 - eps_modes) * eps_dir
 	h, lam = case.initial_h(epsvec)
-	deps0 = 1e-3
+	deps0 = 1e-4
 	resultnorm = []
 	while epsilon0 <= eps_region[0, 1]:
 		deps = deps0
 		epsilon = epsilon0 + deps
 		epsvec = epsilon * eps_modes * eps_dir + (1 - eps_modes) * eps_dir
+		if case.choice_initial == 'fixed':
+			h, lam = case.initial_h(epsvec)
 		result, h_, lam_ = cv.point(epsvec, case, h, lam, display=False)
 		if result[0] == 1:
 			h = h_.copy()
 			lam = lam_
 			epsilon0 = epsilon
-			resultnorm.append([epsilon0, case.norms(h, 4)])
-			print([epsilon0, case.norms(h, 4)[0]])
+			resultnorm.append([epsilon0, case.norms(h, case.r)])
+			print([epsilon0, case.norms(h, case.r)[0]])
 		else:
 			result_temp = [0, 0]
-			while (not result_temp[0]) and deps >= case.tolmin:
+			while (not result_temp[0]) and deps >= 1e-4:
 				deps = deps / 2.0
 				epsilon = epsilon0 + deps
 				epsvec = epsilon * eps_modes * eps_dir + (1 - eps_modes) * eps_dir
@@ -95,8 +99,8 @@ def main():
 				h = h_.copy()
 				lam = lam_
 				epsilon0 = epsilon
-				resultnorm.append([epsilon0, case.norms(h, 4)])
-				print([epsilon0, case.norms(h, 4)[0], 'step2'])
+				resultnorm.append([epsilon0, case.norms(h, case.r)])
+				print([epsilon0, case.norms(h, case.r)[0], result_temp[1]])
 			else:
 				epsilon0 = eps_region[0, 1] + deps
 	if case.save_results:
@@ -116,10 +120,10 @@ class ConfKAM:
 		self.DictParams = dict_params
 		self.precision = {64: xp.float64, 128: xp.float128}.get(self.precision, xp.float64)
 		self.dv = dv
-		dim = len(self.omega0)
+		self.dim = len(self.omega0)
 		self.omega0 = xp.array(self.omega0, dtype=self.precision)
-		self.zero_ = dim * (0,)
-		ind_nu = dim * (fftfreq(self.n, d=1.0/self.precision(self.n)),)
+		self.zero_ = self.dim * (0,)
+		ind_nu = self.dim * (fftfreq(self.n, d=1.0/self.precision(self.n)),)
 		nu = xp.meshgrid(*ind_nu, indexing='ij')
 		self.norm_nu = LA.norm(nu, axis=0)
 		self.omega0_nu = xp.einsum('i,i...->...', self.omega0, nu)
@@ -128,11 +132,19 @@ class ConfKAM:
 		self.lk = - self.omega0_nu ** 2
 		self.sml_div = 1j * self.omega0_nu
 		self.sml_div = xp.divide(1.0, self.sml_div, where=self.sml_div!=0)
-		ind_phi = dim * (xp.linspace(0.0, 2.0 * xp.pi, self.n, endpoint=False, dtype=self.precision),)
+		ind_phi = self.dim * (xp.linspace(0.0, 2.0 * xp.pi, self.n, endpoint=False, dtype=self.precision),)
 		self.phi = xp.meshgrid(*ind_phi, indexing='ij')
-		self.rescale_fft = self.precision(self.n ** dim)
+		self.rescale_fft = self.precision(self.n ** self.dim)
 		self.threshold *= self.rescale_fft
 		ilk = xp.divide(1.0, self.lk, where=self.lk!=0)
+		if self.finer_grid:
+			ind_nu = self.dim * (fftfreq(2*self.n, d=1.0/self.precision(2*self.n)),)
+			nu = xp.meshgrid(*ind_nu2, indexing='ij')
+			omega0_nu = xp.einsum('i,i...->...', self.omega0, nu)
+			self.lk2 = - omega0_nu ** 2
+			self.pad = self.dim * ((self.n//2, self.n//2),)
+			ind_phi = self.dim * (xp.linspace(0.0, 2.0 * xp.pi, 2*self.n, endpoint=False, dtype=self.precision),)
+			self.phi2 = xp.meshgrid(*ind_phi, indexing='ij')
 		self.initial_h = lambda eps: [- ifftn(fftn(self.dv(self.phi, eps, self.Omega)) * ilk), 0.0]
 
 	def refine_h(self, h, lam, eps):
@@ -156,13 +168,19 @@ class ConfKAM:
 		fft_h = fftn(h)
 		fft_h[xp.abs(fft_h) <= self.threshold] = 0.0
 		h_thresh = ifftn(fft_h)
-		arg_v = self.phi + xp.tensordot(self.Omega, h_thresh, axes=0)
-		err = xp.abs(ifftn(self.lk * fft_h) + self.dv(arg_v, eps, self.Omega) + lam).max()
-		return h, lam, err
+		if self.finer_grid:
+			h = ifftn(ifftshift(xp.pad(fftshift(fft_h), self.pad))) * 2 ** self.dim
+			arg_v = self.phi2 + xp.tensordot(self.Omega, h, axes=0)
+			err = xp.abs(ifftn(self.lk2 * fftn(h)) + self.dv(arg_v, eps, self.Omega) + lam).max()
+		else:
+			arg_v = self.phi + xp.tensordot(self.Omega, h_thresh, axes=0)
+			err = xp.abs(ifftn(self.lk * fft_h) + self.dv(arg_v, eps, self.Omega) + lam).max()
+			# err = self.norms(ifftn(self.lk * fft_h) + self.dv(arg_v, eps, self.Omega) + lam, r=self.r)[0]
+		return h_thresh, lam, err
 
 	def norms(self, h, r=0):
-		ffth = fftn(h) / self.rescale_fft
-		return [xp.sqrt(((1.0 + self.norm_nu ** 2) ** r * xp.abs(ffth) ** 2).sum()), xp.sqrt(xp.abs(ifftn(self.omega0_nu ** r * ffth) ** 2).sum()), xp.sqrt(xp.abs(ifftn(self.Omega_nu ** r * ffth) ** 2).sum())]
+		fft_h = fftn(h)
+		return xp.array([xp.sqrt(((1.0 + self.norm_nu ** 2) ** r * (xp.abs(fft_h) / self.rescale_fft) ** 2).sum()), xp.sqrt(xp.abs(ifftn(self.omega0_nu ** r * fft_h) ** 2).sum()), xp.sqrt(xp.abs(ifftn(self.Omega_nu ** r * fft_h) ** 2).sum())])
 
 if __name__ == "__main__":
 	main()
